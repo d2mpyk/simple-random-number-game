b89d2ee5a8c4ac6a6e27b330f26a1078bbf5b22d
d2mp <79327590+d2mpyk@users.noreply.github.com>
2024-01-17 20:18:25 -0500

    Simple Random Number Game JavaScript Ready
    Una sencilla implementación de Adivina el numero aleatorio diseñado en JavaScript

---
+++ app.js
@@ -0,0 +1,22 @@
+// simple random number game in javascript
+let nombre = prompt('Hola, por favor escribe tu nombre: ');
+alert(`Bienvenido ${nombre.trim().replace(/^\w/, (c) => c.toUpperCase())}, al juego del numero aleatorio. `);
+let numeroLimite = 100;
+let numeroAleatorio = Math.floor(Math.random() * numeroLimite) + 1;
+let intentos = 5;
+
+while (intentos > 0) {
+    let numeroUsuario = prompt(`Adivina el numero aleatorio, tienes ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    if (numeroUsuario == numeroAleatorio) {
+        Alert(`Acertaste el numero aleatorio en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+        break;
+    } else {
+        if (numeroUsuario > numeroAleatorio) {
+            alert('El numero es menor!');
+        } else alert('El numero es mayor!');
+        intentos--;
+        if (intentos == 0) {
+            alert(`Que pena, se agotaron los intentos, el numero es: ${numeroAleatorio}`)
+        }
+    }
+}
